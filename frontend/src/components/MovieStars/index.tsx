0284126b87fb86608aaa642663a25995e3e12a81
moisesmiiranda <moisesmiranda1994@gmail.com>
2022-03-13 15:26:08 -0300

    erro netlify

--- frontend/src/components/MovieStars/index.tsx
+++ frontend/src/components/MovieStars/index.tsx
@@ -37,7 +37,7 @@ function Star({ fill } : StarProps) {
     if (fill === 0) {
         return <StarEmpty />
     }
-    else if (fill == 1) {
+    else if (fill === 1) {
         return <StarFull />
     }
     else {
